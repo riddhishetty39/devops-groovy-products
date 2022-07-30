98174f370fd8e360fc663678c3bb15d5ad5e062e
riddhishetty39 <Riddhi.Shetty@ibm.com>
2022-07-30 15:03:42 +0530

    Commit

---
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -0,0 +1,13 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsGroovyProductsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsGroovyProductsApplication, args)
+	}
+
+}
