095fce9ba17a4db17bba2bc3cd011b9a595812f1
riddhishetty39 <Riddhi.Shetty@ibm.com>
2022-08-03 16:53:30 +0530

    first commit

--- src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -2,10 +2,7 @@ package com.myapp.spring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.SpringApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.blind.annotation.GetMapping
 
 
 @SpringBootApplication
---
+++ src/test/groovy/com/myapp/spring/DevopsGroovyProductsApplicationTest.groovy
@@ -0,0 +1,111 @@
+package com.myapp.spring
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+import static org.junit.jupiter.api.Assertions.assertTrue
+
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+
+import io.restassured.RestAssured
+import io.restassured.response.Response
+
+import static groovy.json.JsonOutput.toJson
+
+import static org.springframework.http.MediaType.APPLICATION_JSON
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+
+import org.spockframework.spring.SpringBean
+
+import org.springframework.beans.factory.annotation.Autowired
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+
+import org.springframework.boot.test.context.SpringBootTest
+
+import org.springframework.boot.test.context.TestConfiguration
+
+import org.springframework.boot.test.mock.mockito.MockBean
+
+import org.springframework.context.annotation.Bean
+
+import org.springframework.http.HttpStatus
+
+import org.springframework.test.web.servlet.MockMvc
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+
+
+import spock.lang.Specification
+
+import spock.mock.DetachedMockFactory
+
+class DevopsGroovyProductsApplicationTests {
+	
+	static API_ROOT = 'http://localhost:9090/products'
+	static productId = 1
+
+//	@BeforeTestClass
+//	static void populateData() {
+//		
+//		Product product1 = new Product(productId: 1,productName: 'Iphone13',
+//			description: 'Iphone13 Pro',price: 75453.5,
+//			starRating:4.7)
+//		
+//		
+//		
+//		Response response =
+//		RestAssured.given().contentType(MediaType.APPLICATION_JSON_VALUE)
+//		.body(product1).post(API_ROOT)
+//		
+//		
+//		
+//}
+	
+
+@Test
+void whenGetAllProductList_thenOk(){
+	final Response response = RestAssured.get(API_ROOT)
+	
+	assertEquals HttpStatus.OK.value(),response.getStatusCode()
+	assertTrue response.as(List.class).size() > 0
+}
+
+@Test
+void whenGetProductById_thenOk(){
+	final Response response = RestAssured.get("$API_ROOT/$productId")
+	
+	assertEquals HttpStatus.OK.value(),response.getStatusCode()
+	Product productResponse = response.as Product.class
+	assertEquals productId,productResponse.getProductId()
+}
+
+
+
+
+
+@Test
+void whenSaveProduct_thenOk(){
+	Product product2 = new Product(productId: 4,productName:'Oneplus10',
+			description: 'Oneplus10 Pro',price: 61453.5,
+			starRating:4.6)
+	final Response response =
+	  RestAssured.given()
+		.contentType(MediaType.APPLICATION_JSON_VALUE)
+		  .body(product2).post(API_ROOT)
+		  
+	assertEquals HttpStatus.OK.value(),response.getStatusCode()
+}
+	
+
+}
